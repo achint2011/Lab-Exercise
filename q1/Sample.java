63e5ea78d054993697ce536fd3db5a2c1e7a14ed
Achint Kumar Trivedi <trivediachint@gmail.com>
2021-03-05 13:29:49 +0530

    My First Commit

---
+++ q1/Sample.java
@@ -0,0 +1,27 @@
+class Sample
+
+{
+
+
+
+public static void main(String args[])
+
+{
+int a=10,b=20,sum;
+sum=a+b;
+
+
+System.out.printf("Sum of a and b is %d",sum);
+
+
+
+
+
+
+}
+
+
+
+
+
+}
---
+++ q2/Compare.java
@@ -0,0 +1,19 @@
+class Compare
+{
+public static void main(String args[])
+{
+int a=10,b=15,c=7;
+if(a>b && a>c)
+{
+System.out.printf("%d is largest",a);
+}
+if(b>a && b>c)
+{
+System.out.printf("%d is largest",b);
+}
+if(c>a && c>b)
+{
+System.out.printf("%d is largest",c);
+}
+}
+}
